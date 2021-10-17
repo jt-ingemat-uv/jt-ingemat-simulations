2e934a54de9f9c83422a09d72de8337d15a296d7
jt-ingemat-uv <67114519+jt-ingemat-uv@users.noreply.github.com>
2021-10-17 14:33:27 -0300

    Update simulation.php

--- simulation.php
+++ simulation.php
@@ -283,7 +283,7 @@ if (isset($_POST['calculate'])) {
             $_inf_qs = ceil(pow(10, $year_min[2]));
             $_sup_qs = floor(pow(10, $year_max[2])); 
             
-            $result = "You have a ".number_format($m_pr, 2)." probability to belong to cluster ".$select_color." The centroid for this cluster is XX and YY, with a range of ".number_format($_inf_qs, 2)." and ".number_format($_sup_qs, 2)." and ".number_format($_inf_year, 2)." and ".number_format($_sup_year,2)." years for the QS rank of Hiring university as assistant professor, and hiring year as assistant professor since the graduation year, respectively.";
+            $result = "You have a ".number_format($m_pr, 2)." probability to belong to cluster ".$select_color.".\n The centroid for this cluster is XX and YY, with a range of ".number_format($_inf_qs, 2)." and ".number_format($_sup_qs, 2)." and ".number_format($_inf_year, 2)." and ".number_format($_sup_year,2)." years for the QS rank of Hiring university as assistant professor, and hiring year as assistant professor since the graduation year, respectively.";
         }
     }else{
         $vista_resultado = TRUE;
